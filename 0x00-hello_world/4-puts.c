b8b49aa17aa9f1ecc2be0236817ab9c6722f1601
Dr Prime <gabrielshoyombo2002@gmail.com>
2023-10-08 18:15:48 -0700

    4 puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,10 @@
-#!/bin/bash
 #include <stdio.h>
+
+/**
+ * main - Entry point, prints a specific string
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
     puts("Programming is like building a multilingual puzzle");
