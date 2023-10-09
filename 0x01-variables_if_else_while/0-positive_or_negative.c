ae82ced74750513702335c25bdac2f484788c8de
Dr Prime <gabrielshoyombo2002@gmail.com>
2023-10-08 19:35:23 -0700

    again

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -9,9 +9,12 @@ n = rand() - RAND_MAX / 2;
 /* Print the number */
 printf("%d ", n);
 /* Determine and print if the number is positive, zero, or negative */
-if (n > 0) printf("is positive\n");
-else if (n == 0) printf("is zero\n");
-else printf("is negative\n");
+if (n > 0)
+printf("is positive\n");
+else if (n == 0) 
+printf("is zero\n");
+else 
+printf("is negative\n");
 return (0);
 }
 
